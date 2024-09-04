5a39f545bf2ff5d7b35f0e5b06b994aa9588d8be
Kush Juvekar <119339573+Koosh0610@users.noreply.github.com>
2024-09-04 16:03:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,12 +45,19 @@ def driver_config(args):
 
     if args.force_device_scale:
         options.add_argument("--force-device-scale-factor=1")
+        
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("headless")
-    options.add_argument("--headless=new")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")
+    options.add_argument('--ignore-certificate-errors')
     options.add_argument(
             "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
         )
+    
     options.add_experimental_option(
         "prefs", {
             "download.default_directory": args.download_dir,
